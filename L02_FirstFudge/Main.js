56ea47e7b3feafce3f9d6640f9940fdc30382c79
keckluis <34217915+keckluis@users.noreply.github.com>
2019-10-15 17:49:03 +0200

    15.10.2019

--- L02_FirstFudge/Main.js
+++ L02_FirstFudge/Main.js
@@ -1,12 +1,25 @@
 "use strict";
 var L02_FirstFudge;
 (function (L02_FirstFudge) {
-    var f = FudgeCore;
+    var fudge = FudgeCore;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         const canvas = document.querySelector("canvas");
-        console.log(canvas);
-        let viewport = new f.Viewport();
+        fudge.RenderManager.initialize();
+        fudge.Debug.log(canvas);
+        let node = new fudge.Node("Quad");
+        let mesh = new fudge.MeshQuad();
+        let cmpMesh = new fudge.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        let mtrSolidWhite = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 1, 1, 1)));
+        let cmpMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+        let cam = new fudge.ComponentCamera();
+        cam.pivot.translateZ(2);
+        let viewport = new fudge.Viewport();
+        viewport.initialize("Viewport", node, cam, canvas);
+        fudge.Debug.log(viewport);
+        viewport.draw();
     }
 })(L02_FirstFudge || (L02_FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -1,15 +1,32 @@
 namespace L02_FirstFudge {
 
-    import f = FudgeCore;
+    import fudge = FudgeCore;
 
     window.addEventListener("load", handleLoad);
 
+    export let viewport: fudge.Viewport;
+
     function handleLoad(_event: Event): void {
 
-        const canvas = document.querySelector("canvas");
-        console.log(canvas);
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        fudge.RenderManager.initialize();
 
-        let viewport: f.Viewport = new f.Viewport();
+        fudge.Debug.log(canvas);
         
+        let node: fudge.Node = new fudge.Node("Quad");
+        let mesh: fudge.MeshQuad = new fudge.MeshQuad();
+        let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+
+        let mtrSolidWhite: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 1, 1, 1)));
+        let cmpMaterial: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+        let cam: fudge.ComponentCamera = new fudge.ComponentCamera();
+        cam.pivot.translateZ(2);
+        let viewport: fudge.Viewport = new fudge.Viewport();
+        viewport.initialize("Viewport", node, cam, canvas);
+        fudge.Debug.log(viewport);
+
+        viewport.draw();
     }
 }
