bf95b1fd30c4a29f98d082290b496f93e871a238
keckluis <34217915+keckluis@users.noreply.github.com>
2019-10-17 15:20:27 +0200

    Pong: added bat controls

--- L02_FirstFudge/Main.js
+++ L02_FirstFudge/Main.js
@@ -7,19 +7,61 @@ var L02_FirstFudge;
         const canvas = document.querySelector("canvas");
         fudge.RenderManager.initialize();
         fudge.Debug.log(canvas);
-        let node = new fudge.Node("Quad");
-        let mesh = new fudge.MeshQuad();
-        let cmpMesh = new fudge.ComponentMesh(mesh);
-        node.addComponent(cmpMesh);
-        let mtrSolidWhite = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 1, 1, 1)));
-        let cmpMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
-        node.addComponent(cmpMaterial);
+        let mainNode = new fudge.Node("Main");
+        let batLeft = new fudge.Node("Quad1");
+        let batRight = new fudge.Node("Quad2");
+        let ball = new fudge.Node("Ball");
+        let batLeftMesh = new fudge.MeshQuad();
+        let batLeftCmpMesh = new fudge.ComponentMesh(batLeftMesh);
+        let batRightMesh = new fudge.MeshQuad();
+        let batRightCmpMesh = new fudge.ComponentMesh(batRightMesh);
+        let ballMesh = new fudge.MeshQuad();
+        let ballCmpMesh = new fudge.ComponentMesh(ballMesh);
+        mainNode.appendChild(batLeft);
+        mainNode.appendChild(batRight);
+        mainNode.appendChild(ball);
+        batLeft.addComponent(batLeftCmpMesh);
+        batRight.addComponent(batRightCmpMesh);
+        ball.addComponent(ballCmpMesh);
+        let mtrSolidWhite = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(0, 1, 1, 1)));
+        let batLeftMat = new fudge.ComponentMaterial(mtrSolidWhite);
+        let batRightMat = new fudge.ComponentMaterial(mtrSolidWhite);
+        let ballMat = new fudge.ComponentMaterial(mtrSolidWhite);
+        batLeft.addComponent(batLeftMat);
+        batRight.addComponent(batRightMat);
+        ball.addComponent(ballMat);
+        batLeftCmpMesh.pivot.scaleX(0.1);
+        batLeftCmpMesh.pivot.scaleY(0.5);
+        batLeftCmpMesh.pivot.translateX(-1);
+        batRightCmpMesh.pivot.scaleX(0.1);
+        batRightCmpMesh.pivot.scaleY(0.5);
+        batRightCmpMesh.pivot.translateX(1);
+        ballCmpMesh.pivot.scaleX(0.075);
+        ballCmpMesh.pivot.scaleY(0.075);
         let cam = new fudge.ComponentCamera();
         cam.pivot.translateZ(2);
         let viewport = new fudge.Viewport();
-        viewport.initialize("Viewport", node, cam, canvas);
+        viewport.initialize("Viewport", mainNode, cam, canvas);
         fudge.Debug.log(viewport);
         viewport.draw();
+        document.body.onkeydown = function (e) {
+            if (e.keyCode == 87) {
+                batLeftCmpMesh.pivot.translateY(0.01);
+                viewport.draw();
+            }
+            else if (e.keyCode == 83) {
+                batLeftCmpMesh.pivot.translateY(-0.01);
+                viewport.draw();
+            }
+            if (e.keyCode == 38) {
+                batRightCmpMesh.pivot.translateY(0.01);
+                viewport.draw();
+            }
+            else if (e.keyCode == 40) {
+                batRightCmpMesh.pivot.translateY(-0.01);
+                viewport.draw();
+            }
+        };
     }
 })(L02_FirstFudge || (L02_FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -12,21 +12,80 @@ namespace L02_FirstFudge {
         fudge.RenderManager.initialize();
 
         fudge.Debug.log(canvas);
-        
-        let node: fudge.Node = new fudge.Node("Quad");
-        let mesh: fudge.MeshQuad = new fudge.MeshQuad();
-        let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
-        node.addComponent(cmpMesh);
+        let mainNode: fudge.Node = new fudge.Node("Main");
+        let batLeft: fudge.Node = new fudge.Node("Quad1");
+        let batRight: fudge.Node = new fudge.Node("Quad2");
+        let ball: fudge.Node = new fudge.Node("Ball");
 
-        let mtrSolidWhite: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 1, 1, 1)));
-        let cmpMaterial: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
-        node.addComponent(cmpMaterial);
+        let batLeftMesh: fudge.MeshQuad = new fudge.MeshQuad();
+        let batLeftCmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(batLeftMesh);
+
+        let batRightMesh: fudge.MeshQuad = new fudge.MeshQuad();
+        let batRightCmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(batRightMesh);
+
+        let ballMesh: fudge.MeshQuad = new fudge.MeshQuad();
+        let ballCmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(ballMesh);
+
+        mainNode.appendChild(batLeft);
+        mainNode.appendChild(batRight);
+        mainNode.appendChild(ball);
+
+        batLeft.addComponent(batLeftCmpMesh);
+        batRight.addComponent(batRightCmpMesh);
+        ball.addComponent(ballCmpMesh);
+
+        let mtrSolidWhite: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(0, 1, 1, 1)));
+        let batLeftMat: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+        let batRightMat: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+        let ballMat: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+
+        batLeft.addComponent(batLeftMat);
+        batRight.addComponent(batRightMat);
+        ball.addComponent(ballMat);
+
+        batLeftCmpMesh.pivot.scaleX(0.1);
+        batLeftCmpMesh.pivot.scaleY(0.5);
+        batLeftCmpMesh.pivot.translateX(-1);
+
+        batRightCmpMesh.pivot.scaleX(0.1);
+        batRightCmpMesh.pivot.scaleY(0.5);
+        batRightCmpMesh.pivot.translateX(1);
+
+        ballCmpMesh.pivot.scaleX(0.075);
+        ballCmpMesh.pivot.scaleY(0.075);
+
         let cam: fudge.ComponentCamera = new fudge.ComponentCamera();
         cam.pivot.translateZ(2);
+
         let viewport: fudge.Viewport = new fudge.Viewport();
-        viewport.initialize("Viewport", node, cam, canvas);
+        viewport.initialize("Viewport", mainNode, cam, canvas);
         fudge.Debug.log(viewport);
 
         viewport.draw();
+
+        document.body.onkeydown = function (e) {
+
+            if (e.keyCode == 87) {
+
+                batLeftCmpMesh.pivot.translateY(0.01);
+                viewport.draw();
+
+            } else if (e.keyCode == 83) {
+
+                batLeftCmpMesh.pivot.translateY(-0.01);
+                viewport.draw();
+            }
+
+            if (e.keyCode == 38) {
+
+                batRightCmpMesh.pivot.translateY(0.01);
+                viewport.draw();
+                
+            } else if (e.keyCode == 40) {
+
+                batRightCmpMesh.pivot.translateY(-0.01);
+                viewport.draw();
+            }
+        };
     }
 }
